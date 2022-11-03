aa812a368f88d7a1d1b533e474ab61897e759e5a
Ben Kaminski <ben@benkaminski.com>
2022-11-03 19:43:47 -0400

    added navbar and CSS

--- functions/theme-dependencies.php
+++ functions/theme-dependencies.php
@@ -1,7 +1,7 @@
 <?php 
 //LOAD SCRIPTS
 function enqueue_dmi_scripts() {
-    wp_enqueue_script('Bootstrap-5.0', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js', array('jquery'), '', true);
+    wp_enqueue_script('Bootstrap-5.2.2', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js', array('jquery'), '', true);
     wp_enqueue_script('font-awesome-5', 'https://kit.fontawesome.com/42219cbebc.js', false, null, null, false);
     wp_enqueue_script('bkc-scripts', get_template_directory_uri() . '/assets/js/bkcScripts.min.js', array('jquery'), null, true, null);
 }
@@ -9,7 +9,7 @@ add_action('wp_enqueue_scripts', 'enqueue_dmi_scripts');
 
 //LOAD CSS
 function enqueue_dmi_styles() {
-    wp_enqueue_style('bootstrap-5.0', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap-5.2.2', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css');
     wp_enqueue_style('bkc-styles', get_template_directory_uri() . '/assets/styles/bkcStyles.min.css');
 }
 add_action('wp_enqueue_scripts', 'enqueue_dmi_styles');
--- parts/page-main-nav.php
+++ parts/page-main-nav.php
@@ -1,25 +1,25 @@
 <header>
-	<nav class="navbar navbar-expand-lg fixed-top shadow" style="background: #fff;">
+	<nav class="navbar navbar-expand-lg fixed-top shadow navbar-dark bg-primary">
 		<div class="container">
-			<a class="navbar-brand" href="<?php echo get_home_url(); ?>" >
-				<img src="<?php echo get_home_url(); ?>/wp-content/uploads/" alt="" class="img-fluid">	
+			<a class="navbar-brand" href="<?php echo get_home_url(); ?>">
+				<img src="<?php echo get_home_url(); ?>/wp-content/uploads/BK-LOGO.png" width="75" alt="<?php echo get_bloginfo( 'name' ); ?>" class="img-fluid">	
 			</a>
-			<button class="navbar-toggler rounded-0" type="button" data-bs-toggle="collapse" data-bs-target="#dmiNav" aria-controls="dmiNav" aria-expanded="false" aria-label="Toggle navigation">
+			<button class="navbar-toggler rounded-0" type="button" data-bs-toggle="collapse" data-bs-target="#bkcNav" aria-controls="bkcNav" aria-expanded="false" aria-label="Toggle navigation">
 				<i class="fas fa-bars fa-2x"></i>
 			</button>
-			<div class="collapse navbar-collapse text-uppercase font-weight-bold" id="dmiNav">
+			<div class="collapse navbar-collapse text-uppercase font-weight-bold" id="bkcNav">
 				<?php
-				wp_nav_menu( array(
-					'theme_location'    => 'primary',
-					'depth'             => 2,
-					'container'         => '',
-					'container_class'   => '',
-					'container_id'      => '',
-					'menu_class'        => 'navbar-nav ms-auto',
-					'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-					'walker'            => new WP_Bootstrap_Navwalker(),
-				)); ?>
-				
+					wp_nav_menu( array(
+						'theme_location'    => 'primary',
+						'depth'             => 2,
+						'container'         => '',
+						'container_class'   => '',
+						'container_id'      => '',
+						'menu_class'        => 'navbar-nav ms-auto',
+						'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+						'walker'            => new WP_Bootstrap_Navwalker(),
+					)); 
+				?>
 			</div>
 		</div>
 	</nav>
