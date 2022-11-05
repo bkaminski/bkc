f1b0fcf5b5390f60b94660d1c5f591c1ebcb3674
Ben Kaminski <ben@benkaminski.com>
2022-11-04 20:43:01 -0400

    more hero styles

--- parts/home-hero.php
+++ parts/home-hero.php
@@ -2,6 +2,7 @@
     <div class="inner-text">
         <p class="p1">YOUR</p>
         <p class="p2">DEVELOPER</p>
+        <p class="p3 h5 display-6">Benjamin Kaminski Consulting</p>
     </div>
     <a class="hero-down" href="#this">
         <div class="btn-react"></div>
